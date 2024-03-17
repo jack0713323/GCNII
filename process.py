836086cd6b4243fb90af3110bd38520b5fe6e054
jack0713323 <90665172+jack0713323@users.noreply.github.com>
2024-03-17 20:59:29 +0800

    Add files via upload

--- process.py
+++ process.py
@@ -26,7 +26,7 @@ def sample_mask(idx, l):
     """Create mask."""
     mask = np.zeros(l)
     mask[idx] = 1
-    return np.array(mask, dtype=np.bool)
+    return np.array(mask, dtype=bool)
 
 
 def full_load_citation(dataset_str):
